3cddd45ed9d3e1664d858c568cb1c990e290526c
Jonathan Cushing <firex42069@gmail.com>
2022-03-25 00:26:18 -0400

    make high score screen accessible

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-// text
+var bodyEl = document.querySelector("body");
+var articleEl = document.querySelector("article");
+var startingPageEl = document.querySelector("#starting-page");
+var question2El = document.querySelector("#question-2");
+var question3El = document.querySelector("#question-3");
+var question4El = document.querySelector("#question-4");
+var question5El = document.querySelector("#question-5");
+var finishScreenEl = document.querySelector("#finish-screen");
+var highScoreEl = document.querySelector("#high-scores")
+/*var rightOrWrong = document.createElement("h2");
+
+rightOrWrong.textContent = "Correct!";
+
+question2El.appendChild(rightOrWrong);*/
+
+var buttonHandler = function(event) {
+    var targetEl = event.target;
+
+    if (targetEl.matches("#view-high-scores")) {
+        articleEl.className = "display-none";
+        highScoreEl.classList.remove("display-none");
+    }
+    else if (targetEl.matches("#go-back")) {
+        highScoreEl.className = "display-none";
+        startingPageEl.classList.remove("display-none");
+    }
+};
+
+var submitScore = function() {
+    event.preventDefault();
+};
+
+bodyEl.addEventListener("click", buttonHandler);
+//finishScreenEl.addEventListener("submit", submitScore);

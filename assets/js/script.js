644c66a4c5c4e92363504df06c4e99b13b3fb0fd
Jonathan Cushing <firex42069@gmail.com>
2022-03-27 21:37:39 -0400

    localStorage; clear high scores // createScoreItem bugged

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ var finishScreenEl = document.querySelector("#finish-screen");
 var scoreEl = document.querySelector("#score");
 var formEl = document.querySelector("#submit-score");
 var highScoreEl = document.querySelector("#high-scores");
+var scoreListEl = document.querySelector("#score-list");
+var scoreItemEl = document.createElement("li");
 var questionContent = [
     "<h1>Commonly used data types do not include:</h1><ul><li class='wrong'>1. strings</li><li class='right'>2. alerts</li><li class='wrong'>3. booleans</li><li class='wrong'>4. numbers</li></ul>",
     "<h1>The condition in an if / else statement is enclosed with ________.</h1><ul><li class='wrong'>1. quotes</li><li class='wrong'>2. curly brackets</li><li class='right'>3. parentheses</li><li class='wrong'>4. square brackets</li></ul>",
@@ -14,6 +16,8 @@ var questionContent = [
     "<h1>String values must be enclosed within ________ when being assigned to variables.</h1><ul><li class='wrong'>1. commas</li><li class='wrong'>2. curly brackets</li><li class='right'>3. quotes</li><li class='wrong'>4. parentheses</li></ul>",
     "<h1>A very useful tool used during development and debugging for printing content to the debugger is:</h1><ul><li class='wrong'>1. JavaScript</li><li class='wrong'>2. terminal / bash</li><li class='wrong'>3. for loops</li><li class='right'>4. console.log</li></ul>"
 ];
+var scores = [];
+var scoreIdCounter = 0;
 var questionNumber = 1;
 var timeLeft = 0;
 var finalScore = 0;
@@ -33,6 +37,12 @@ var buttonHandler = function(event) {
         highScoreEl.className = "display-none";
         startingPageEl.classList.remove("display-none");
     }
+    else if (targetEl.matches("#clear")) {
+        localStorage.removeItem("scores");
+        scores = [];
+        scoreIdCounter = 0;
+        scoreListEl.innerHTML = "";
+    }
     else if (targetEl.matches("#start")) {
         startingPageEl.className = "display-none";
         questionContainerEl.classList.remove("display-none");
@@ -51,10 +61,10 @@ var buttonHandler = function(event) {
     else if (targetEl.matches(".wrong")) {
         checkAnswerEl.remove();
         questionNumber++;
+        timeLeft = timeLeft - 10;
         quizHandler();
         checkAnswerEl.textContent = "Wrong!";
         questionContainerEl.appendChild(checkAnswerEl);
-        timeLeft = timeLeft - 10;
     }
 };
 
@@ -91,7 +101,6 @@ var timer = function() {
 var submitScore = function() {
     event.preventDefault();
     var nameInput = document.querySelector("input[name='name']").value;
-    var scoreListEl = document.querySelector("#score-list");
 
     if (!nameInput) {
         alert("You need to provide your initials in order to submit your score!");
@@ -108,11 +117,37 @@ var submitScore = function() {
     finishScreenEl.className = "display-none";
     highScoreEl.classList.remove("display-none");
 
-    var scoreItemEl = document.createElement("li");
-    scoreItemEl.setAttribute("score", scoreItem.score);
+    createScoreItem(scoreItem);
+};
+
+var createScoreItem = function(scoreItem) {
+    scoreItemEl.setAttribute("score-id", scoreIdCounter);
     scoreItemEl.textContent = scoreItem.name + " - " + scoreItem.score;
     scoreListEl.appendChild(scoreItemEl);
+
+    scores.push(scoreItem);
+    scoreIdCounter++;
+
+    saveScores();
+};
+
+var saveScores = function() {
+    localStorage.setItem("scores", JSON.stringify(scores));
+};
+
+var loadScores = function() {
+    var savedScores = localStorage.getItem("scores");
+    if (!savedScores) {
+        return false;
+    }
+
+    savedScores = JSON.parse(savedScores);
+
+    for (var i = 0; i < savedScores.length; i++) {
+        createScoreItem(savedScores[i]);
+    }
 };
 
 bodyEl.addEventListener("click", buttonHandler);
 formEl.addEventListener("submit", submitScore);
+loadScores();
